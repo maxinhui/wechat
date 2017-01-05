79d3df98f0082695d9e57aa07ddd25c9cdaa6237
maxinhui <850244962@qq.com>
2017-01-05 17:28:08 +0800

    shiro測試

--- wechat-business/src/main/java/top/builbu/business/user/service/impl/UserRealm.java
+++ wechat-business/src/main/java/top/builbu/business/user/service/impl/UserRealm.java
@@ -3,26 +3,30 @@ package top.builbu.business.user.service.impl;
 
 	import java.util.Set;
 
-	import org.apache.shiro.authc.AuthenticationException;
-	import org.apache.shiro.authc.AuthenticationInfo;
-	import org.apache.shiro.authc.AuthenticationToken;
-	import org.apache.shiro.authc.LockedAccountException;
-	import org.apache.shiro.authc.SimpleAuthenticationInfo;
-	import org.apache.shiro.authc.UnknownAccountException;
-	import org.apache.shiro.authz.AuthorizationInfo;
-	import org.apache.shiro.authz.SimpleAuthorizationInfo;
-	import org.apache.shiro.realm.AuthorizingRealm;
-	import org.apache.shiro.subject.PrincipalCollection;
+import lombok.extern.slf4j.Slf4j;
 
-	import org.springframework.beans.factory.annotation.Autowired;
-	   
+	import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+	@Slf4j   
 	public class UserRealm extends AuthorizingRealm {
 
 		@Override
-		protected AuthorizationInfo doGetAuthorizationInfo(
-				PrincipalCollection principals) {
-			// TODO Auto-generated method stub
-			return null;
+		protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+			 String username = (String) principals.getPrimaryPrincipal();  
+			  log.info(username);
+		        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo(); 
+		        
+		        
+		        return authorizationInfo;  
 		}
 
 		@Override
